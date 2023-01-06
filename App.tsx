a8559fddd6ba115e8a1329f7fb1acfc488e81b5f
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-06 14:35:12 +0530

    setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
-import A from './components/A/A';
-import A1 from './components/A1/A1';
-import './style.css';
+import './style.scss';
 
 export default function App() {
-  return (
-    <div>
-      <A1 />
-    </div>
-  );
+  return <div>Hello</div>;
 }
