9fbfa1977033d73ea7cc098a2ab598d48519fb7c
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-12 01:03:39 +0530

    2 files modified

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import "./Header.css"
+import './Header.css';
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">CRUD</div>
+      <div className="logo">REACT CRUD</div>
       <div className="links">Home</div>
     </header>
   );
