1310d5b6095bf2c68fc3d4208726e3729269aaaf
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-12 21:16:28 +0530

    2 files modified, 3 deleted

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
-import A from './components/A/A';
-import A1 from './components/A1/A1';
 import './style.css';
+import AgoraRTC from 'agora-rtc-sdk-ng';
 
 export default function App() {
+  let options = {
+    appId: '4004b0616f2f45eebb8656f1423f9846',
+    channel: 'easy-solutions',
+    token:
+      '007eJxTYOBU6HG+G3Il8JpUP58Ae3G39bQVkm4tZzr+PT5R2yj0nVOBwcTAwCTJwMzQLM0ozcQ0NTUpycLM1CzN0MTIOM3SwsTsnvCB5IZARoaGd2KsjAwQCOLzMaQmFlfqFufnlJZk5ucVMzAAAAK4IfI=',
+    uid: 0,
+  };
+
   return (
     <div>
-      <A1 />
+      <button>Join</button>
+      <br />
+      <br />
+      <button>Leave</button>
     </div>
   );
 }
