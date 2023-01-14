61d60dfc5e4cfc55f8993ba23fe5ce3d3a61338b
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-15 01:26:17 +0530

    3 files modified, 2 added

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../..';
+import ChangeColor, { color } from '../ChangeColor/ChangeColor';
+import "./Profile.css"
 
 const Profile = () => {
   const user = useSelector((state: RootState) => state.user.value);
+  const themeColor:color = useSelector((state:RootState)=>state.theme.value);
   return (
-    <div className="profile">
+    <div className={themeColor.name===`dark`?`profile dark`:`profile`}>
       <h3>Profile</h3>
+      <ChangeColor />
       <p>Name-:{user.name}</p>
       <p>Age-:{user.age}</p>
       <p>Email-:{user.email}</p>
--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import userReducer from './feature/user';
+import themeReducer from './feature/theme';
 
 import App from './App';
 
@@ -13,9 +14,10 @@ const root = createRoot(rootElement);
 const store = configureStore({
   reducer: {
     user: userReducer,
+    theme: themeReducer,
   },
 });
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 root.render(
   <StrictMode>
     <Provider store={store}>
