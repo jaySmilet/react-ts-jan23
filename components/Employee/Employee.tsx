94ed8cb5982fe4a9359b205a65a48084b3f080b6
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-16 12:40:07 +0530

    1 file modified

--- components/Employee/Employee.tsx
+++ components/Employee/Employee.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
-import "./Employee.css";
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+import './Employee.css';
 
 const Employee = () => {
+  const employees = useSelector((state) => state.value);
   return (
     <div className="employee">
       <div className="display">
-        <p>Username</p>
+        <p>Username: {}</p>
         <p>Name</p>
       </div>
     </div>
