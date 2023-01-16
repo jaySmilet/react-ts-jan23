4026d6df3d85610af8ae0138ad2b2c33d51d623f
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-16 12:33:40 +0530

    1 file modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
+import Employee from './components/Employee/Employee';
 import './style.css';
-
-
 
 export default function App() {
   return (
     <div className="app">
+      <Employee />
     </div>
   );
 }
---
+++ components/Employee/Employee.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./Employee.css";
+
+const Employee = () => {
+  return (
+    <div className="employee">
+      <div className="display">
+        <p>Username</p>
+        <p>Name</p>
+      </div>
+    </div>
+  );
+};
+
+export default Employee;
