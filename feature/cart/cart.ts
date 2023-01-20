3bc879d6adb4905cc7d5a20c4e5214686d02fa6a
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2023-01-21 00:51:33 +0530

    1 file modified, 1 added

--- feature/cart/cart.ts
+++ feature/cart/cart.ts
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { cartItems } from '../../static/cartOrigin';
 
 const initialStateValue = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
---
+++ static/cartOrigin.ts
@@ -0,0 +1,38 @@
+export const cartItems: Cart[] = [
+  {
+    id: 'asdf123',
+    title: 'one',
+    price: 40,
+    img: '',
+    amount: 1,
+  },
+  {
+    id: 'asdf124',
+    title: 'two',
+    price: 30,
+    img: '',
+    amount: 2,
+  },
+  {
+    id: 'asdf125',
+    title: 'three',
+    price: 40,
+    img: '',
+    amount: 1,
+  },
+  {
+    id: 'asdf125',
+    title: 'four',
+    price: 40,
+    img: '',
+    amount: 1,
+  },
+];
+
+export interface Cart {
+  id: string;
+  title: string;
+  price: number;
+  img: string;
+  amount: number;
+}
